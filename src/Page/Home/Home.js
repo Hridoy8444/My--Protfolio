33169d9424fe7e6c811eb042d4b8ecc3f16cd62e
fardous hridoy <mdreday736@gmail.com>
2022-06-09 15:48:06 +0600

    protfolio start

---
+++ src/Page/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+            <p>Hello, I am</p>
+            <h1 className='text-7xl font-bold'>Fardous Hossain</h1>
+            <p>A Young MERN stack Web Developer, Front-end Web Developer </p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
